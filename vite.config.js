45d0cbbe947f32155afe94201d0d3655f217c1f0
unknown <taewoo1212@gmail>
2023-12-31 21:43:18 +0900

    board 페이지 초기 작업중

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,14 @@
-import { resolve } from 'node:path'
-import { defineConfig } from 'vite'
-
-
-
-
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   build: {
-    outDir:'docs',
+    outDir: 'docs',
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        
+        board: resolve(__dirname, 'src/pages/board/index.html'),
       },
     },
-    
   },
-})
+});
