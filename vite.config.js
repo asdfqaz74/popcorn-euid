dc6589dadfaee4830743e4998f2ec5f24b580016
asdfqaz74 <74591618+asdfqaz74@users.noreply.github.com>
2024-01-06 03:19:14 +0900

    feat : search 페이지 구현
    - 선택자 class 추가
    - tailwind css 컴포넌트 추가
    
    #62

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         story: resolve(__dirname, 'src/pages/story/index.html'),
+        search: resolve(__dirname, 'src/pages/search/index.html'),
       },
     },
   },
