cce0d3234691f66aa13918f67d98b9e94c2817fb
iamjinpark <wldls7754@gmail.com>
2024-01-08 22:16:54 +0900

    modify : html 마크업 리팩토링 - iamjinpark

--- src/pages/category/category.js
+++ src/pages/category/category.js
@@ -12,7 +12,7 @@ goBack.addEventListener('click', handleButton);
 // 플러스 버튼 클릭 시 호버 효과
 const category = document.querySelectorAll('.category-item-button');
 
-function toggleImage(e) {
+function toggleImage() {
   const plusImage = this.querySelector('.category-item-image');
   const isChecked = document.getElementById('checkbox').checked;
 
@@ -23,9 +23,10 @@ function toggleImage(e) {
   }
 }
 
-// for (let i = 0; i < category.length; i++) {
-//   category[i].addEventListener('click', toggleImage);
-// }
+for (let i = 0; i < category.length; i++) {
+  category[i].addEventListener('click', toggleImage);
+}
+
 // category.forEach(function (plus) {
 //   plus.addEventListener('click', toggleImage);
 // });
