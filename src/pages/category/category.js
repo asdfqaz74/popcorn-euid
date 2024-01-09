b95491a479eaa28b1967139c69d73f07a7057b04
iamjinpark <wldls7754@gmail.com>
2024-01-10 03:26:29 +0900

    modify : 마크업 수정 및 hover 이벤트 추가 - iamjinpark

--- src/pages/category/category.js
+++ src/pages/category/category.js
@@ -10,25 +10,17 @@ function handleButton() {
 goBack.addEventListener('click', handleButton);
 
 // 플러스 버튼 클릭 시 배경 변경
-// const categoryItems = document.querySelectorAll('.category-item');
+const categorySave = document.querySelectorAll('.save');
 
-// function changeItem() {
-//   this.classList.toggle('category-button-selected');
+function changeItem(e) {
+  e.stopPropagation();
+  const label = this.closest('label');
+  label.classList.toggle('category-button-selected');
+  label.classList.toggle('bg-primary');
+}
 
-//   const image = this.querySelector('.category-item-image');
-
-//   if (image) {
-//     const currentImage = image.style.backgroundImage;
-
-//     image.style.backgroundImage = currentImage.includes('checked.svg')
-//       ? "url('/public/images/plus.svg')" // 이미 'checked.svg'라면 'plus.svg'로 변경
-//       : "url('/public/images/checked.svg')"; // 그렇지 않으면 'checked.svg'로 변경
-
-//     image.style.backgroundRepeat = 'no-repeat';
-//   }
-// }
-// categoryItems.forEach(function (item) {
-//   item.addEventListener('click', changeItem);
-// });
+categorySave.forEach(function (item) {
+  item.addEventListener('click', changeItem);
+});
 
 // localStorage 에 선택된 값 저장하기
