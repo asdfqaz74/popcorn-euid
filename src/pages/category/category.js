a38890c71a0784d09e476560590957dc20bf6734
iamjinpark <wldls7754@gmail.com>
2024-01-10 13:37:10 +0900

    feat : 클릭한 관심분야 localStorage로 저장 -iamjinpark

--- src/pages/category/category.js
+++ src/pages/category/category.js
@@ -1,4 +1,4 @@
-import { getNode, addClass } from '/src/lib/';
+import { getNode } from '/src/lib/';
 
 // 뒤로가기 버튼 클릭 시 이전 페이지로 이동
 const goBack = getNode('.button-goback');
@@ -10,17 +10,46 @@ function handleButton() {
 goBack.addEventListener('click', handleButton);
 
 // 플러스 버튼 클릭 시 배경 변경
-const categorySave = document.querySelectorAll('.save');
+const categoryInputs = document.querySelectorAll('.category-checkbox');
 
-function changeItem(e) {
-  e.stopPropagation();
+function changeItemBg() {
   const label = this.closest('label');
   label.classList.toggle('category-button-selected');
   label.classList.toggle('bg-primary');
+  // console.log(e.target);
 }
 
-categorySave.forEach(function (item) {
-  item.addEventListener('click', changeItem);
+categoryInputs.forEach(function (item) {
+  item.addEventListener('click', changeItemBg);
 });
 
 // localStorage 에 선택된 값 저장하기
+
+function saveInterest(event) {
+  const getItem = localStorage.getItem('interest');
+  const value = event.target.id;
+
+  // 저장된 데이터가 있는 경우에는 파싱하여 배열로 변환
+  let interest = [];
+  if (getItem) {
+    interest = JSON.parse(getItem);
+  }
+
+  // 클릭된 input 요소의 id 값을 저장하거나 제거
+  const index = interest.indexOf(value);
+  if (index === -1) {
+    interest.push(value);
+  } else {
+    interest.splice(index, 1);
+  }
+
+  // 배열을 문자로 변환 후 로컬스토리지에 저장
+  const jsonString = JSON.stringify(interest);
+  localStorage.setItem('interest', jsonString);
+}
+
+categoryInputs.forEach(function (item) {
+  item.addEventListener('click', saveInterest);
+});
+
+//
