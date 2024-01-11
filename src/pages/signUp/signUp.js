8eb4e32903e8df605b9dae1ccd30ebc1562bc21a
iamjinpark <wldls7754@gmail.com>
2024-01-12 03:15:52 +0900

    feat : valid 및 로컬스토리지 저장 구현 완료 - iamjinpark 01/12 3:16am

--- src/pages/signUp/signUp.js
+++ src/pages/signUp/signUp.js
@@ -1,6 +1,7 @@
-import { removeClass } from '../../lib/dom/css';
+import { addClass, removeClass } from '../../lib/dom/css';
 import { getNode, toggleClass } from '/src/lib/';
 import PocketBase from 'pocketbase';
+import pb from '/src/api/pocketbase';
 
 /* -------------------------------------------------------------------------- */
 /*                               버튼으로 페이지 이동                              */
@@ -9,7 +10,6 @@ const goBack = getNode('.button-goBack');
 const moveBack = getNode('.button-moveBack');
 const signUpContainer = getNode('.signUp-container');
 const signUpFormBefore = getNode('.signUp-form-before');
-const agree = getNode('.signUp-button-agree');
 
 // 첫번째 페이지 뒤로가기
 function handleButton() {
@@ -27,9 +27,6 @@ signUpFormBefore.addEventListener('submit', (e) => {
   e.preventDefault();
   signUpContainer.style.transform = 'translateX(-50%)';
 });
-agree.addEventListener('click', () => {
-  window.location.href = '/src/pages/story/';
-});
 
 /* -------------------------------------------------------------------------- */
 /*                         휴대폰 번호 valid 상태로 변경                            */
@@ -37,7 +34,7 @@ agree.addEventListener('click', () => {
 // 1. input.value랑 정규식 활용하여 유효성 검사
 // 2. 조건에 충족하면 버튼에 'signUp-verify-valid' 클래스 추가
 
-const phoneNumberInput = document.getElementById('phonenumber');
+const phoneNumberInput = document.getElementById('phoneNumber');
 const verifyButton = getNode('.signUp-button-verify');
 const agreeButton = document.getElementById('agree');
 const regex = /^\d{3}\d{4}\d{4}$/;
@@ -61,6 +58,8 @@ phoneNumberInput.addEventListener('input', validCheckPhoneNumber);
 /*                              인증번호 받아오기                                 */
 /* -------------------------------------------------------------------------- */
 const randomNumber = Math.floor(Math.random() * 900000) + 100000;
+const setVerifyNumber = sessionStorage.setItem('verifyNumber', randomNumber);
+const getVerifyNumber = sessionStorage.getItem('verifyNumber');
 
 function handelverifyNumber() {
   const buttonValid = Array.from(verifyButton.classList).includes(
@@ -68,8 +67,9 @@ function handelverifyNumber() {
   );
 
   if (buttonValid) {
-    localStorage.setItem('verifyNumber', randomNumber);
-    alert(randomNumber);
+    alert(getVerifyNumber);
+    // 인증번호 비교 위해 콘솔로 불러오기 -> 로컬이라 변수 설정 다시 하기!
+    console.log(getVerifyNumber);
   }
 }
 
@@ -81,7 +81,7 @@ verifyButton.addEventListener('click', handelverifyNumber);
 
 function validPhoneNumber() {
   const phoneNumberValue = getNode('.signUp-input-phoneNumber').value;
-  // console.log(phoneNumberValue);
+  console.log(phoneNumberValue);
   const sendPhoneNumber = JSON.stringify(phoneNumberValue);
 
   localStorage.setItem('phoneNumber', sendPhoneNumber);
@@ -97,3 +97,64 @@ verifyButton.addEventListener('click', validPhoneNumber);
 /* -------------------------------------------------------------------------- */
 /*                             입력번호 유효성 검사                                */
 /* -------------------------------------------------------------------------- */
+
+const verifyNumberInput = getNode('.signUp-input-verifyNumber');
+
+function ValidVerifyNumber(e) {
+  const verifyNumber = e.target.value;
+  console.log(verifyNumber);
+
+  if (getVerifyNumber === verifyNumber) {
+    console.log('성공!');
+    removeClass(agreeButton, 'bg-gray-500');
+    toggleClass(agreeButton, 'signUp-agree-valid');
+  } else {
+    console.log('실패!');
+    removeClass(agreeButton, 'signUp-agree-valid');
+    addClass(agreeButton, 'bg-gray-500');
+  }
+}
+
+verifyNumberInput.addEventListener('input', ValidVerifyNumber);
+
+/* -------------------------------------------------------------------------- */
+/*                            유효성 검사 끝나고 이동                               */
+/* -------------------------------------------------------------------------- */
+function allValidCheck() {
+  const agreeButtonValid = Array.from(agreeButton.classList).includes(
+    'signUp-agree-valid'
+  );
+
+  if (agreeButtonValid) {
+    const userName = Math.floor(Math.random() * 1000) + 1000;
+    const phoneNumber = localStorage.getItem('phoneNumber');
+
+    const data = {
+      username: userName,
+      phoneNumber: phoneNumber,
+    };
+
+    pb.collection('users').create(data);
+    window.location.href = '/src/pages/story/';
+  } else {
+    alert('인증번호가 잘못되었습니다.');
+  }
+}
+
+agreeButton.addEventListener('click', allValidCheck);
+
+/* -------------------------------------------------------------------------- */
+/*                              pb로 데이터 전송                                 */
+/* -------------------------------------------------------------------------- */
+
+// const pb = new PocketBase('https://popcorns.pockethost.io');
+
+// function sendData(){
+// const data = {
+//   userName: userName,
+//   phoneNumber: phoneNumber,
+// };
+
+// const record = await pb.collection('users').create(data);}
+
+// agreeButton.addEventListener('click', sendData);
