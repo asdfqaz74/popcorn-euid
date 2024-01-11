c7d13bc68b8d540119fdb2f4c6cc8603878bbb14
iamjinpark <wldls7754@gmail.com>
2024-01-11 13:27:00 +0900

    feat : 핸드폰 번호 valid 및 localStorage 저장 기능 구현 - iamjinpark

--- src/pages/signUp/signUp.js
+++ src/pages/signUp/signUp.js
@@ -1,26 +1,81 @@
-import { getNode } from '/src/lib/';
+import { removeClass } from '../../lib/dom/css';
+import { getNode, toggleClass } from '/src/lib/';
+import PocketBase from 'pocketbase';
 
-// 뒤로가기 버튼 클릭 시 이전페이지로 이동
-const goBack = getNode('.button-goback');
+/* -------------------------------------------------------------------------- */
+/*                               버튼으로 페이지 이동                              */
+/* -------------------------------------------------------------------------- */
+const goBack = getNode('.button-goBack');
+const moveBack = getNode('.button-moveBack');
+const signUpContainer = getNode('.signUp-container');
+const signUpFormBefore = getNode('.signUp-form-before');
+const agree = getNode('.signUp-button-agree');
 
+// 첫번째 페이지 뒤로가기
 function handleButton() {
   history.back();
 }
 
+// 두번째 페이지 뒤로가기
+function handlePageButton() {
+  signUpContainer.style.transform = 'translateX(0%)';
+}
+
 goBack.addEventListener('click', handleButton);
-
-// 버튼 클릭시 다음페이지로 이동
-const signUpContainer = document.querySelector('.signUp-container');
-const signUpFormBefore = document.querySelector('.signUp-form-before');
-
+moveBack.addEventListener('click', handlePageButton);
 signUpFormBefore.addEventListener('submit', (e) => {
-  e.preventDefault(); // 기본 동작 중단
-
-  signUpContainer.style.transform = 'translateX(-50%)'; // 첫 번째 div 왼쪽으로 이동
+  e.preventDefault();
+  signUpContainer.style.transform = 'translateX(-50%)';
+});
+agree.addEventListener('click', () => {
+  window.location.href = '/src/pages/story/';
 });
 
-const agreeButton = document.getElementById('Agree');
+/* -------------------------------------------------------------------------- */
+/*                         휴대폰 번호 valid 상태로 변경                            */
+/* -------------------------------------------------------------------------- */
+// 1. input.value랑 정규식 활용하여 유효성 검사
+// 2. 조건에 충족하면 버튼에 'signUp-verify-valid' 클래스 추가
 
-agreeButton.addEventListener('click', () => {
-  window.location.href = '/src/pages/story/'; // /src/pages/main/으로 페이지 이동
-});
+const phoneNumberInput = document.getElementById('PhoneNumber');
+const verifyButton = getNode('.signUp-button-verify');
+const agreeButton = document.getElementById('agree');
+
+function validCheckPhoneNumber(e) {
+  const phoneNumber = e.target.value;
+  console.log(phoneNumber);
+  const regex = /^\d{3}\d{4}\d{4}$/;
+  const isValidPhoneNumber = regex.test(phoneNumber);
+
+  if (isValidPhoneNumber) {
+    removeClass(verifyButton, 'text-gray-500');
+    toggleClass(verifyButton, 'signUp-verify-valid');
+  } else {
+    removeClass(verifyButton, 'signUp-verify-valid');
+  }
+}
+
+phoneNumberInput.addEventListener('input', validCheckPhoneNumber);
+
+/* -------------------------------------------------------------------------- */
+/*              입력한 휴대폰 번호값 localStorage에 저장하고 화면에 랜더링               */
+/* -------------------------------------------------------------------------- */
+
+function validPhoneNumber() {
+  const phoneNumberValue = getNode('.signUp-input-phoneNumber').value;
+  // console.log(phoneNumberValue);
+  const sendPhoneNumber = JSON.stringify(phoneNumberValue);
+
+  localStorage.setItem('phoneNumber', sendPhoneNumber);
+  console.log('저장 완료');
+
+  const showPhoneNumber = getNode('.signUp-input-after');
+  const getPhoneNumber = localStorage.getItem('phoneNumber');
+  showPhoneNumber.textContent = JSON.parse(getPhoneNumber);
+}
+
+verifyButton.addEventListener('click', validPhoneNumber);
+
+/* -------------------------------------------------------------------------- */
+/*                              인증번호 받아오기                                 */
+/* -------------------------------------------------------------------------- */
