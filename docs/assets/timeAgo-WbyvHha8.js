2a3acea4784fd7ca02c3e2c4e3e2d39d51762534
vHwav <148796897+vHwav@users.noreply.github.com>
2024-01-16 09:13:46 +0900

    Delete docs directory
    최종 배포 위한 docs 파일 삭제

--- docs/assets/timeAgo-WbyvHha8.js
+++
@@ -1,1 +0,0 @@
-function d(o){const f=new Date(o),t=(new Date-f)/1e3,n=t/60,e=n/60,r=e/24;return t<60?Math.round(t)+"초 전":n<60?Math.round(n)+"분 전":e<24?Math.round(e)+"시간 전":Math.round(r)+"일 전"}export{d as t};
