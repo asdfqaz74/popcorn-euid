05757ab43aa58bf1b008ddf101a858c004bc0a70
unknown <taewoo1212@gmail>
2024-01-25 01:15:21 +0900

    featuer: 검색기능 범위추가 및 board에도 연결  +commnity taewook 01/25 오전1:15

--- src/pages/search/search.js
+++ src/pages/search/search.js
@@ -2,7 +2,7 @@ import { getNode, insertLast, removeChild, debounce } from '/src/lib';
 import pb from '/src/api/pocketbase';
 import searchIcon from '/public/images/search.svg';
 
-function handleInput(e, productData) {
+function handleInput(e, combinedData) {
   let value = e.target.value;
 
   if (!value) {
@@ -10,27 +10,48 @@ function handleInput(e, productData) {
     return;
   }
 
-  let filteredData = productData.filter((item) => item.title.includes(value));
+  const filteredData = combinedData.filter((item) => {
+    const titleMatch = item.title && item.title.includes(value);
+    const activityMatch = item.activity && item.activity.includes(value);
+
+    return titleMatch || activityMatch;
+  });
 
   removeChild('.search-info');
 
   filteredData.forEach((item) => {
-    const template = /* html */ `
-    <div>
-    <a href="${`/src/pages/exchangeBoard/index.html#${item.id}`}"
-    class="flex border-b gap-2 mb-2">
-    <img src="${searchIcon}" alt="" />
-    <span>${item.title}</span>
-    </a>
-    </div>
-    `;
+    if (!item.SR_location) {
+      const template = /* html */ `
+          <div>
+          <a href="${`/src/pages/exchangeBoard/index.html#${item.id}`}"
+          class="flex border-b gap-2 mb-2">
+          <img src="${searchIcon}" alt="" />
+          <span>${item.title}</span>
+          </a>
+          </div>
+          `;
 
-    insertLast('.search-info', template);
+      insertLast('.search-info', template);
+    } else {
+      const template = /* html */ `
+          <div>
+          <a href="${`/src/pages/boardContent/index.html#${item.id}`}"
+          class="flex border-b gap-2 mb-2">
+          <img src="${searchIcon}" alt="" />
+          <span>${item.title}</span>
+          </a>
+          </div>
+          `;
+      insertLast('.search-info', template);
+    }
   });
 }
 
 async function init() {
+  const communityData = await pb.collection('community').getFullList();
   const productData = await pb.collection('products').getFullList();
+
+  const combinedData = communityData.concat(productData);
 
   const back = getNode('.search-back');
   const input = getNode('#searchInput');
@@ -38,7 +59,7 @@ async function init() {
 
   input.addEventListener(
     'input',
-    debounce((e) => handleInput(e, productData), 500)
+    debounce((e) => handleInput(e, combinedData), 500)
   );
 }
 
